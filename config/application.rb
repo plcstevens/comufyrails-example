04c811f8fb54490f35916b5cf29216fedc37b6d2
philip <philip@tauri-tec.com>
2013-01-10 15:24:04 +0000

    config.assets.initialize_on_precompile = false needs to be disabled for Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module RailsExample
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Disabled to avoid exceptions on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
