87f5a3a65727c7735aa4460f145a43853cdbf99e
philip <philip@tauri-tec.com>
2013-01-14 15:07:49 +0000

    Fixed up a number of bugs and errors with how this example worked, it is now cleaner and less likely to put in invalid data (like nils)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,40 +5,47 @@ class User < ActiveRecord::Base
   validates :fact,        :presence => true
   validates :dob,         :timeliness => { :on_or_before => :today, :type => :date }
 
-  # Queries this user against Comufy, and updates the database accordingly.
-  def update_with_comufy
-    Comufyrails::Connection.user(facebook_id) do |account, tags, other|
-      facebook_id = account["fbId"]
-      dob         = tags["dob"]
-      fact        = tags["Fact"]
-
-      user = User.where("facebook_id = ?", facebook_id).first
-      if user
-        user.dob  = dob
-        user.fact = fact
-        user.save!
-      end
-    end
-  end
-
   # Goes through all users, checking their data and updating it.
   def self.update_with_comufy
     Comufyrails::Connection.users do |users, total, to, from|
       users.each do |user|
+        # each user is a hash containing two keys; account and tagValues.
+        # account contains two keys "name" and "fbId".
         account = user.delete("account")
-        tags    = user.delete("tagValues")
+        name    = account.delete("name")
+        fbid    = account.delete("fbId")
 
-        facebook_id = account["fbId"]
-        dob         = tags["dob"]
-        fact        = tags["Fact"]
+        # tagValues contains every tag for this user, including any custom ones you have set.
+        tags = user.delete("tagValues")
+        dob  = tags.delete("dob")
+        fact = tags.delete("fact")
 
-        our_user = User.where("facebook_id = ?", facebook_id).first
-        if our_user
-          p "Found our user, let us update its entries!"
-          our_user.dob  = dob
-          our_user.fact = fact
-          our_user.save!
+        user = User.where("facebook_id = ?", fbid).first
+        if user
+          user.dob  = dob   if dob
+          user.fact = fact  if fact
+          user.save
         end
+      end
+    end
+  end
+
+  # Queries this user against Comufy, and updates the database accordingly.
+  def update_with_comufy
+    Comufyrails::Connection.user(self.facebook_id) do |account, tags, other|
+
+      # just to confirm we have the right account check these values before saving
+      name = account.delete("name")
+      fbid = account.delete("fbId")
+
+      # get our unique data tags
+      tag_dob  = tags.delete("dob")
+      tag_fact = tags.delete("fact")
+
+      if fbid == self.facebook_id
+        self.dob  = tag_dob  if tag_dob
+        self.fact = tag_fact if tag_fact
+        self.save
       end
     end
   end
